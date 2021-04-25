de3d007e276dececb0f7f6f2da37deb5371b3372
令晨 孟 <mlc@aheadx.com>
2021-04-25 10:10:31 +0800

    1111

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
-    <p>11123</p>
+    <p>1112334</p>
     <router-view></router-view>
   </div>
 </template>
